a738b54612d9c8142c2aaac09cf97c4ff644ad6b
Bankole Oluwole <bankolemail@gmil.com>
2016-10-17 16:00:16 -0700

    loaded wine prefabs

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemManager : MonoBehaviour {
+
+	string WINE_PREFAB = "wine";
+	Vector3[] wineLocations;
+
+	// Use this for initialization
+	void Start () {
+		LoadWines ();
+	}
+
+	void LoadWines (){
+		LoadWineLocations ();
+
+		for (int i = 0; i < wineLocations.Length; i++) {
+			LoadWine (i);
+		}
+	}
+
+	void LoadWineLocations (){
+		wineLocations = new Vector3[4];
+		wineLocations [0] = new Vector3 (1, -1.5f);
+		wineLocations [1] = new Vector3 (1, 1.5f);
+		wineLocations [2] = new Vector3 (1, -1.5f);
+		wineLocations [3] = new Vector3 (-1, -1.5f);
+	}
+
+
+	void LoadWine (int i){
+		Instantiate (Resources.Load (WINE_PREFAB), wineLocations [i], Quaternion.identity);
+	}
+
+}
---
+++ Assets/Scripts/items/Wine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wine : MonoBehaviour {
+
+	int RESPAWN_TIME = 300; //5 secs
+	int respawn_countdown;
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (GameSetup.isHost) {
+			if (col.gameObject.name == "player" || col.gameObject.name == "enemy") {
+				//hide
+				//send to client to hide
+				//show loading bubbles
+				//add to speed
+			}
+		}
+	}
+
+	void Update(){
+		//if beer is hidden, start countdown
+		//after countdown, reattach beer
+			//remove loading animation
+			//reset countdown
+	}
+}
